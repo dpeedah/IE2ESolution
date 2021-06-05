2ed42a7a7b9ab96e5b89eed1d0cd0c065cf321eb
dpeedah <dan.peedah@gmail.com>
2021-06-05 13:57:43 +0100

    sonarqube changes

---
+++ src/test/java/helperpackage/fileHandlerTest.java
@@ -0,0 +1,34 @@
+package helperpackage;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class fileHandlerTest {
+
+    @Test
+    public void testGetInputs(){
+        List<String> inputs = new ArrayList<String>();
+        try {
+           inputs = TestValueExtractor.getRegInputValuesCustom();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int inputSize = inputs.size();
+        Assert.assertTrue(inputSize > 0);
+    }
+
+    @Test
+    public void testGetOutputs(){
+        List<Car> cars = new ArrayList<Car>();
+        try {
+            cars = TestValueExtractor.getOutputValues();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int inputSize = cars.size();
+        Assert.assertTrue(inputSize > 0);
+    }
+}
